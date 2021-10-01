3a2d20fb3854b120d868dd04fbce8969a330b2d0
samoeun chhay <samoeunchh@gmail.com>
2021-10-01 14:00:40 +0700

    Add project files.

---
+++ SR413.API/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR413.API.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid BrandId { get; set; }
+        [Required(ErrorMessage ="Brand Name field is required")]
+        [MaxLength(50)]
+        public string BrandName { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+    }
+}
